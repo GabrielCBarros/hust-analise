62f5b65ba462ff4e8e5674908fb76584ba3602f3
Gabriel Cerqueira Barros <gabrielcbarros11@gmail.com>
2024-05-05 17:17:11 -0300

    leitor Json

--- chat-ai/index.ts
+++ chat-ai/index.ts
@@ -1,40 +1,14 @@
-import {fileURLToPath} from "url";
-import path from "path";
-import {LlamaModel, LlamaContext, LlamaChatSession} from "node-llama-cpp";
+import fs from "fs";
 
-// model name
-const MODEL_NAME = "mistral-7b-instruct-v0.2.Q5_K_M.gguf";
-// const MODEL_NAME = "llama-2-7b.Q5_K_M.gguf";
+function fetchJSONData() {
+  fs.readFile("./Mensagens/messagens.lucas.json", "utf8", (error, data) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    let json = JSON.parse(data)
+    console.log(json.mensagens[5])
+  });
+}
 
-// get the models directory
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const modelsDirectory = path.join(__dirname, "../models");
-const modelsPath = path.join(modelsDirectory, MODEL_NAME);
-
-
-const model = new LlamaModel({
-    modelPath : modelsPath
-});
-
-const context = new LlamaContext({model});
-const session = new LlamaChatSession({context});
-
-const warmupPrompt = "who would win at arm wrestling: pikachu or mr tickle?";
-console.log("Human: " + warmupPrompt);
-
-const warmupResponse = await session.prompt(warmupPrompt);
-
-// show the results
-console.log("AI: " + warmupResponse);
-
-const prompt = "Answer in one word.  What is the opposite of true?";
-console.log("Human: " + prompt);
-
-const startTime = Date.now();
-const response = await session.prompt(prompt);
-const endTime = Date.now();
-
-// show the results
-console.log("AI: " + response);
-console.log(`Execution Time: ${endTime - startTime} ms`);
+fetchJSONData();
